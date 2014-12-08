e65a48034f36895212c3ef424b5ef23c741fecc7
Aidan Lister <aidan@aidanlister.com>
2014-12-09 10:50:37 +1100

    The last commit had the right idea but the wrong implementation. This ensures both template1 and postgres databases have the hstore running under the same eid.

--- create_extension.sh
+++ create_extension.sh
@@ -1,3 +1,11 @@
+# Because both template1 and the user postgres database have already been created,
+# we need to create the hstore extension in template1 and then recreate the postgres database.
+#
+# Running CREATE EXTENSION in both template1 and postgres can lead to
+# the extensions having different eid's.
 gosu postgres postgres --single template1 -E <<-EOSQL
 	CREATE EXTENSION hstore;
+	DROP DATABASE postgres;
+	CREATE DATABASE postgres TEMPLATE template1;
 EOSQL
+
